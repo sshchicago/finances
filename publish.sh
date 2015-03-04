e500a0e2542aa0f2df2c05484a53fa5ca4ed283d
christopheragocs <cagocs@backstopsolutions.com>
2015-03-04 13:03:04 -0600

    made publish less drastic

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ YEAR=`date +%Y`
 ledger -f $YEAR-$MONTH.dat b > cash.txt
 
 if [ $? -eq 0 ]
-then git add -A; git commit -m "looks good to me!"; git push origin master
+then git add cash.txt; git commit -m "looks good to me!"; git push origin master
 fi
