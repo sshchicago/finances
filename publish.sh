26141dbb1bb23f62125439ae008c272166120d9d
Chris Agocs <camerazn@gmail.com>
2016-08-17 19:41:32 -0500

    Update the publishing script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,6 @@
-MONTH=`date +%m`
 YEAR=`date +%Y`
 
-ledger -f $YEAR-$MONTH.dat b > cash.txt
+ledger -f fy$YEAR.dat -p "this month" b > cash.txt
 
 if [ $? -eq 0 ]
 then git add cash.txt; git commit -m "looks good to me!"; git push origin master
