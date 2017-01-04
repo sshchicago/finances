2203897ff9cc27afb5afaa44a551ca99cdab4d68
Dmitriy Vysotskiy <dvysotskiy@sshchicago.org>
2017-01-04 08:19:26 +0000

    december balance

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 YEAR=`date +%Y`
 
-ledger -f fy$YEAR.dat -p "this month" b > cash.txt
+ledger -f fy$YEAR.dat -p "last month" b > cash.txt
 
 if [ $? -eq 0 ]
 then git add cash.txt; git commit -m "looks good to me!"; git push origin master
